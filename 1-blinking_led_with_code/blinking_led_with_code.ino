0cc3d83e07b830b4ad96d46b68b3eb054dc1cdd8
Isgnderx <ayxanisgenderzade999@gmail.com>
2026-01-18 21:59:51 +0400

    Update blinking_led_with_code.ino

--- 1-blinking_led_with_code/blinking_led_with_code.ino
+++ 1-blinking_led_with_code/blinking_led_with_code.ino
@@ -1,14 +1,28 @@
-// C++ code
-//
+// Blinking LED with Code
+// This code turns an LED ON and OFF every 1 second using Arduino
+
 void setup()
 {
+  // Set digital pin 7 as OUTPUT
+  // This pin will send voltage to the LED
   pinMode(7, OUTPUT);
 }
 
 void loop()
 {
+  // Send HIGH signal (5V) to pin 7
+  // This turns the LED ON
   digitalWrite(7, HIGH);
+
+  // Wait for 1000 milliseconds (1 second)
   delay(1000);
+
+  // Send LOW signal (0V) to pin 7
+  // This turns the LED OFF
   digitalWrite(7, LOW);
+
+  // Wait for 1000 milliseconds (1 second)
   delay(1000);
+
+  // After this, the loop starts again automatically
 }
